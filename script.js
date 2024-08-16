618dde2b01a9b91f9f7bb7e0ca27e4be664e8257
Zenonace4 <anthonyjecaat@gmail.com>
2024-08-16 15:14:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 var swiper = new Swiper(".mySwiper-1", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    }
+  direction: "horizontal",
+	loop: true,
+
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev"
+	},
+
+	autoplay: {
+		delay: 3000
+	},
+
+	pagination: {
+		el: ".swiper-pagination",
+		type: "bullets",
+		clickable: true
+	}
 
 });
 var swiper = new Swiper(".mySwiper-2", {
